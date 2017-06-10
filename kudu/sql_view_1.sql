c9b53907639aa77d9cb5383c1dc7f6537ae41876
Kristian <kristian.sebastian@gmail.com>
2017-06-10 23:42:35 +0200

    View 1 implemeted for Apache kudu

--- kudu/sql_view_1.sql
+++ kudu/sql_view_1.sql
@@ -4,21 +4,20 @@ set var:domain = 'domain1';
 set var:first_date = 1461456000;
 -- 2017-04-26
 set var:last_date = 1493164800;
-set var:tlds = 'es,pt';
+set var:tlds = ('es','pt');
 
 select 
 	g.page_name, g.total_views, g.total_visits, g.average_duration, 
 	max_vs.tld as tld_max_visits,
 	max_br.date_tld as date_tld_max_bounce_rate,
 	min_new_vt.date_tld as date_tld_min_new_visits
-	
 from (
 	-- Create grouped data (total views, total visits and average duration) by page name 
 	select 	page_name, 
 		sum(views) as total_views, sum(visits) as total_visits, avg(average) as average_duration 
 	from pages 
 	where domain = ${var:domain} and ${var:first_date} <= date_stats and date_stats <= ${var:last_date}
-	    -- and find_in_set(tld,'es,pt')
+	    and tld in ${var:tlds}
 	group by page_name
 	order by page_name limit 200) g
 	
@@ -26,34 +25,33 @@ join (
 	-- Create tld with max visits by page name
  	select distinct page_name,tld,visits 
 	from (
-	
 		select page_name,domain,tld,visits, 
 			max(visits) over (partition by page_name,domain) max_visits 
 		from pages 
-		where domain = @domain and @first_date <= date_stats and date_stats <= @last_date and 
-			find_in_set(tld,@tlds)) a 
+        where domain = ${var:domain} and ${var:first_date} <= date_stats and date_stats <= ${var:last_date}
+            and tld in ${var:tlds}) a
 	where a.visits = a.max_visits) max_vs
 	
 join (
 	-- Create date + tld with max bounce rate by page name 
- 	select distinct page_name,concat_ws('/',date_stats,tld) as date_tld,bounce_rate 
+ 	select distinct page_name, concat(from_unixtime(date_stats, 'yyyy-MM-dd'), '/', tld) as date_tld,bounce_rate
 	from (
 		select page_name,domain,date_stats,tld,bounce_rate,
 			max(bounce_rate) over (partition by page_name,domain) max_bounce_rate 
 		from pages 
-		where domain = @domain and @first_date <= date_stats and date_stats <= @last_date and
-			find_in_set(tld,@tlds)) a 
+        where domain = ${var:domain} and ${var:first_date} <= date_stats and date_stats <= ${var:last_date}
+            and tld in ${var:tlds}) a
 	where a.bounce_rate = a.max_bounce_rate) max_br
 	
 join (
 	-- Create date + tld with min visitors by page name 
-	select distinct page_name, concat_ws('/',date_stats,tld) as date_tld, new_visitors 
+	select distinct page_name, concat(from_unixtime(date_stats, 'yyyy-MM-dd'), '/', tld) as date_tld, new_visitors
 	from (
 		select page_name,domain,date_stats,tld,new_visitors,
 			min(new_visitors) over (partition by page_name,domain) min_new_visitors 
 		from pages 
-		where domain = @domain and @first_date <= date_stats and date_stats <= @last_date and
-			find_in_set(tld,@tlds)) a 
+        where domain = ${var:domain} and ${var:first_date} <= date_stats and date_stats <= ${var:last_date}
+            and tld in ${var:tlds}) a
 	where a.new_visitors = a.min_new_visitors) min_new_vt
 
 on 
